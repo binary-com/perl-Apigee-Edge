08623e3b3f2189fa29682ee3f60859c404b90420
Fayland Lam <fayland@gmail.com>
2014-11-30 18:12:25 +0800

    0.06

--- lib/Apigee/Edge.pm
+++ lib/Apigee/Edge.pm
@@ -2,7 +2,7 @@ package Apigee::Edge;
 
 use strict;
 use warnings;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use Carp;
 use Mojo::UserAgent;
--- lib/Apigee/Edge/Helper.pm
+++ lib/Apigee/Edge/Helper.pm
@@ -2,7 +2,7 @@ package Apigee::Edge::Helper;
 
 use strict;
 use warnings;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use Carp;
 use base 'Apigee::Edge';
